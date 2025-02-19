1494e5ad33b131c8a4216217bf1d8f49c337a26f
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-02-19 12:47:43 -0500

    raw func base, build modularity later

--- task.js
+++ task.js
@@ -5,5 +5,19 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 dotenv.config();
 
 class Script{
-    
+    constructor(){
+        this.email = process.env.EMAIL;
+        this.region = process.env.REGION;
+    }
+
+    async firstPage(){
+        
+    }
+
+    async signUp(){
+
+    }
 }
+
+Script();
+Script.start();
