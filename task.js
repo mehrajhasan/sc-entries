9c45066265b2c0b055036c20c3736d8a651bbc9e
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-02-19 12:44:02 -0500

    start files

---
+++ task.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'; 
+import got from 'got';
+import { CookieJar } from 'tough-cookie';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+dotenv.config();
+
+class Script{
+    
+}
